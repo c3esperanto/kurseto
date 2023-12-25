26968fd391233d280b897d40b61fbd77cf602ef2
Johannes Mueller <github@johannes-mueller.org>
2023-12-25 12:00:45 +0100

    Attempt to fix revealjs-url

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pandoc -t revealjs -s -o _site/index.html esperanto-37c3.md -V revealjs-url= --include-in-header=slides.css
+pandoc -t revealjs -s -o _site/index.html esperanto-37c3.md -V revealjs-url=kurseto --include-in-header=slides.css
 cp qr-code.svg _site
